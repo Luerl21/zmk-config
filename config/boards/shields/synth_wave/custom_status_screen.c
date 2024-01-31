5edc5b42933168efd876e9b05b45be916b7ff18a
Luerl21 <dmyt20042@gmail.com>
2024-01-31 19:08:54 +0300

    test custom output status widget

--- config/boards/shields/synth_wave/custom_status_screen.c
+++ config/boards/shields/synth_wave/custom_status_screen.c
@@ -11,7 +11,7 @@
 #include <zmk/display/widgets/wpm_status.h>
 
 #include "custom_status_screen.h"
-#include "widgets/output_status.h"
+// #include "widgets/output_status.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
