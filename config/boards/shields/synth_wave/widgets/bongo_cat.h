d8275cfd468c3a29e1c85071b838319eeed17252
Luerl21 <dmyt20042@gmail.com>
2024-01-31 20:02:26 +0300

    test custom widgets cresk

--- config/boards/shields/synth_wave/widgets/bongo_cat.h
+++ config/boards/shields/synth_wave/widgets/bongo_cat.h
@@ -9,11 +9,11 @@
 #include <lvgl.h>
 #include <zephyr/kernel.h>
 
-struct zmk_widget_output_status {
+struct zmk_widget_bongo_cat {
     sys_snode_t node;
     lv_obj_t *obj;
     bool is_right;
 };
 
-int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_obj_t *parent);
-lv_obj_t *zmk_widget_output_status_obj(struct zmk_widget_output_status *widget);
+int zmk_widget_bongo_cat_init(struct zmk_widget_bongo_cat *widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_bongo_cat_obj(struct zmk_widget_bongo_cat *widget);
