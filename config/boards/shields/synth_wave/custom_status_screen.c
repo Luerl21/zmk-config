22440affe0e01d76f5d558c667c11a50e7d03fd0
Luerl21 <dmyt20042@gmail.com>
2024-02-01 17:44:25 +0300

    test my custom output

--- config/boards/shields/synth_wave/custom_status_screen.c
+++ config/boards/shields/synth_wave/custom_status_screen.c
@@ -40,7 +40,7 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
-    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_MID, 0, 2);
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
@@ -55,15 +55,11 @@ lv_obj_t *zmk_display_status_screen() {
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
-    lv_obj_t *LayersHeading;
-    LayersHeading = lv_img_create(screen);
-    lv_obj_align(LayersHeading, LV_ALIGN_BOTTOM_MID, 0, -30);
-    lv_img_set_src(LayersHeading, &layers2);
 
     zmk_widget_layer_status_init(&layer_status_widget, screen);
     lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
-                               &lv_font_montserrat_16, LV_PART_MAIN);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_MID, 0, -5);
+                               &lv_font_montserrat_12, LV_PART_MAIN);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
 #endif
 
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
