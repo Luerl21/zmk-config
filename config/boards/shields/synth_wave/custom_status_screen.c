5278bd5df42d7f950be8781457007c10b9555e1e
Luerl21 <dmyt20042@gmail.com>
2024-02-01 19:11:58 +0300

    test my custom output

--- config/boards/shields/synth_wave/custom_status_screen.c
+++ config/boards/shields/synth_wave/custom_status_screen.c
@@ -63,7 +63,7 @@ lv_obj_t *zmk_display_status_screen() {
     zmk_widget_layer_status_init(&layer_status_widget, screen);
     lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
                                &lv_font_montserrat_12, LV_PART_MAIN);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 15, 0);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 20, 0);
 #endif
 
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
