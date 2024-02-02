325eecd10bc8f372e2157fa4a1f58e081bd3eece
Luerl21 <dmyt20042@gmail.com>
2024-02-02 15:54:32 +0300

    test

--- config/boards/shields/synth_wave/widgets/peripheral_status.c
+++ config/boards/shields/synth_wave/widgets/peripheral_status.c
@@ -64,27 +64,7 @@ static void set_battery_symbol(lv_obj_t *icon, struct battery_status_state state
                     lv_img_set_src(icon, &batt_0_chg);
                 }
         #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
-        void battery_status_update_cb(struct battery_status_state state) {
-            struct zmk_widget_peripheral_status *widget;
-                SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_battery_symbol(widget->obj, state); }
-        }
-
-        static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
-            return (struct battery_status_state) {
-                .level = zmk_battery_state_of_charge(),
-        #if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-                .usb_present = zmk_usb_is_powered(),
-        #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
-            };
-        }
-
-        ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_status, struct battery_status_state,
-                                    battery_status_update_cb, battery_status_get_state)
-
-        ZMK_SUBSCRIPTION(widget_battery_status, zmk_battery_state_changed);
-        #if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-        ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
-        #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
+        
     } else {
         struct peripheral_status_state {
             bool connected;
