80c8cdc3962a8c4428e7a0e6608492cf3caab799
Luerl21 <36129502+Luerl21@users.noreply.github.com>
2025-03-13 17:45:39 +0300

    my zmk

--- config/boards/shields/synth_wave/custom_status_screen.c
+++ config/boards/shields/synth_wave/custom_status_screen.c
@@ -36,42 +36,42 @@ static struct zmk_widget_peripheral_status peripheral_status_widget;
 static struct zmk_widget_layer_status layer_status_widget;
 #endif
 
-// lv_obj_t *zmk_display_status_screen() {
+lv_obj_t *zmk_display_status_screen() {
 
-//     lv_obj_t *screen;
-//     screen = lv_obj_create(NULL);
+    lv_obj_t *screen;
+    screen = lv_obj_create(NULL);
 
-// #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
-//     zmk_widget_battery_status_init(&battery_status_widget, screen);
-//     lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
+    zmk_widget_battery_status_init(&battery_status_widget, screen);
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
     
-//     zmk_widget_peripheral_battery_status_init(&battery_status_peripheral_widget, screen);
-//     lv_obj_align(zmk_widget_peripheral_battery_status_obj(&battery_status_peripheral_widget), LV_ALIGN_BOTTOM_LEFT, 25, 0);
-// #endif
+    zmk_widget_peripheral_battery_status_init(&battery_status_peripheral_widget, screen);
+    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&battery_status_peripheral_widget), LV_ALIGN_BOTTOM_LEFT, 25, 0);
+#endif
 
-// #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
-//     zmk_widget_output_status_init(&output_status_widget, screen);
-//     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
-// #endif
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
+    zmk_widget_output_status_init(&output_status_widget, screen);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
+#endif
 
-// #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
-//     zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
-//     lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_CENTER, 0, 0);
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
+    zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
+    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_CENTER, 0, 0);
     
-// #endif
+#endif
 
-// #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
 
-//     lv_obj_t *LayersHeading;
-//     LayersHeading = lv_img_create(screen);
-//     lv_obj_align(LayersHeading, LV_ALIGN_TOP_LEFT, 0, 0);
-//     lv_img_set_src(LayersHeading, &layers);
+    lv_obj_t *LayersHeading;
+    LayersHeading = lv_img_create(screen);
+    lv_obj_align(LayersHeading, LV_ALIGN_TOP_LEFT, 0, 0);
+    lv_img_set_src(LayersHeading, &layers);
 
-//     zmk_widget_layer_status_init(&layer_status_widget, screen);
-//     lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
-//                                &lv_font_montserrat_12, LV_PART_MAIN);
-//     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 20, 0);
-// #endif
+    zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
+                               &lv_font_montserrat_12, LV_PART_MAIN);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 20, 0);
+#endif
 
-//     return screen;
-// }
+    return screen;
+}
